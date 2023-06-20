410500ae23b43e134e0cad1ce210dac31ab29318
chao.zhang <chao.zhang@sophgo.com>
2023-06-20 18:25:15 +0800

    fix bytetrack download
    Change-Id: Id9987285bec80f93c43f6b41d16be22900c32553

--- samples/bytetrack/scripts/download.sh
+++ samples/bytetrack/scripts/download.sh
@@ -6,15 +6,24 @@ scripts_dir=$(dirname $(readlink -f "$0"))
 # echo $scripts_dir
 
 pushd $scripts_dir
-# datasets
-if [ ! -d "../data" ];
-then
-    python3 -m dfn --url http://219.142.246.77:65000/sharing/wEeqHde0i
-    unzip data.zip -d ../
-    rm data.zip
 
-else
-    echo "data exist!"
-fi
+mkdir -p ../data
+
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/5bYERv4FR
+unzip models.zip
+rm -rf models.zip
+mv ./models ../data/
+
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/yd6kbSu7n
+unzip videos.zip
+rm -rf videos.zip
+mv ./videos ../data/
+
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/6DFPVVKsd
+mv ./coco.names ../data/
 
 popd
+
+
+
+
