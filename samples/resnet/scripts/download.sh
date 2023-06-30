d1f2b931b4a0fcad1fda116f0bc8272957bce960
yifan.wang <yifan.wang@sophgo.com>
2023-06-30 11:52:37 +1000

    feat(resnet): add resnet usecase
    Change-Id: Iae09bd8bf5013832cb10ae9fdf0dd6c7d0939228

---
+++ samples/resnet/scripts/download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pip3 install dfn
+
+scripts_dir=$(dirname $(readlink -f "$0"))
+# echo $scripts_dir
+
+pushd $scripts_dir
+
+mkdir -p ../data
+
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/PqECRJ0sR
+unzip models.zip
+rm -rf models.zip
+mv ./models ../data/
+
+# python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/MYS5paD3l
+# unzip videos.zip
+# rm -rf videos.zip
+# mv ./videos ../data/
+
+# python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/6DFPVVKsd
+# mv ./coco.names ../data/
+
+popd
---
+++ samples/resnet/src/resnet_demo.cc
@@ -0,0 +1,131 @@
+#include <sys/stat.h>
+
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <opencv2/opencv.hpp>
+#include <unordered_map>
+
+#include "common/clocker.h"
+#include "common/error_code.h"
+#include "common/logger.h"
+#include "common/object_metadata.h"
+#include "common/profiler.h"
+#include "decode.h"
+#include "engine.h"
+#include "init_engine.h"
+
+typedef struct demo_config_ {
+  int num_graphs;
+  int num_channels_per_graph;
+  nlohmann::json channel_config;
+  bool download_image;
+  std::string engine_config_file;
+} demo_config;
+
+constexpr const char* JSON_CONFIG_NUM_CHANNELS_PER_GRAPH_FILED =
+    "num_channels_per_graph";
+constexpr const char* JSON_CONFIG_ENGINE_CONFIG_PATH_FILED =
+    "engine_config_path";
+constexpr const char* JSON_CONFIG_CHANNEL_CONFIG_FILED = "channel";
+
+demo_config parse_demo_json(std::string& json_path) {
+  std::ifstream istream;
+  istream.open(json_path);
+  assert(istream.is_open());
+  nlohmann::json demo_json;
+  istream >> demo_json;
+  istream.close();
+
+  demo_config config;
+
+  config.num_channels_per_graph =
+      demo_json.find(JSON_CONFIG_NUM_CHANNELS_PER_GRAPH_FILED)->get<int>();
+
+  auto channel_config_it = demo_json.find(JSON_CONFIG_CHANNEL_CONFIG_FILED);
+  config.channel_config = *channel_config_it;
+
+  config.engine_config_file =
+      demo_json.find(JSON_CONFIG_ENGINE_CONFIG_PATH_FILED)->get<std::string>();
+
+  return config;
+}
+
+int main() {
+  ::logInit("debug", "");
+
+  std::mutex mtx;
+  std::condition_variable cv;
+
+  sophon_stream::common::Clocker clocker;
+  std::atomic_uint32_t frameCount(0);
+  std::atomic_int32_t finishedChannelCount(0);
+
+  auto& engine = sophon_stream::framework::SingletonEngine::getInstance();
+
+  std::ifstream istream;
+  nlohmann::json engine_json;
+  std::string resnet_config_file = "../config/resnet_demo.json";
+  demo_config resnet_json = parse_demo_json(resnet_config_file);
+
+  // 启动每个graph, graph之间没有联系，可以是完全不同的配置
+  istream.open(resnet_json.engine_config_file);
+  assert(istream.is_open());
+  istream >> engine_json;
+  istream.close();
+
+  resnet_json.num_graphs = engine_json.size();
+  int num_channels =
+      resnet_json.num_channels_per_graph * resnet_json.num_graphs;
+  ::sophon_stream::common::FpsProfiler fpsProfiler("fps_resnet_demo", 100);
+  auto sinkHandler = [&](std::shared_ptr<void> data) {
+    // write stop data handler here
+    auto objectMetadata =
+        std::static_pointer_cast<sophon_stream::common::ObjectMetadata>(data);
+    if (objectMetadata == nullptr) return;
+    frameCount++;
+    if (objectMetadata->mFrame->mEndOfStream) {
+      printf("meet a eof\n");
+      finishedChannelCount++;
+      if (finishedChannelCount == num_channels) {
+        cv.notify_one();
+      }
+      return;
+    }
+    fpsProfiler.add(1);
+  };
+
+  std::map<int, std::pair<int, int>> graph_src_id_port_map;
+  init_engine(engine, engine_json, sinkHandler, graph_src_id_port_map);
+
+  for (auto graph_id : engine.getGraphIds()) {
+    for (int channel_id = 0; channel_id < resnet_json.num_channels_per_graph;
+         ++channel_id) {
+      nlohmann::json channel_config = resnet_json.channel_config;
+      channel_config["channel_id"] = channel_id;
+      auto channelTask =
+          std::make_shared<sophon_stream::element::decode::ChannelTask>();
+      channelTask->request.operation = sophon_stream::element::decode::
+          ChannelOperateRequest::ChannelOperate::START;
+      channelTask->request.json = channel_config.dump();
+      std::pair<int, int> src_id_port = graph_src_id_port_map[graph_id];
+      sophon_stream::common::ErrorCode errorCode =
+          engine.pushSourceData(graph_id, src_id_port.first, src_id_port.second,
+                                std::static_pointer_cast<void>(channelTask));
+    }
+  }
+
+  {
+    std::unique_lock<std::mutex> uq(mtx);
+    cv.wait(uq);
+  }
+  for (int i = 0; i < resnet_json.num_graphs; i++) {
+    std::cout << "graph stop" << std::endl;
+    engine.stop(i);
+  }
+  long totalCost = clocker.tell_us();
+  std::cout << " total time cost " << totalCost << " us." << std::endl;
+  double fps = static_cast<double>(frameCount) / totalCost;
+  std::cout << "frame count is " << frameCount << " | fps is " << fps * 1000000
+            << " fps." << std::endl;
+  return 0;
+}
