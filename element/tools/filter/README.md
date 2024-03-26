8c5b940c6aaaecf8f440f78cce548dc119b500ae
yuchuan.he <yuchuan.he@sophgo.com>
2024-03-26 21:26:48 +0800

    add(element):add filter element
    Change-Id: Ib7ad4854f8c7740ae06855e08d388928cbe8a9fa

---
+++ element/tools/filter/README.md
@@ -0,0 +1,78 @@
+# sophon-stream http_push element
+
+[English](README_EN.md) | 简体中文
+
+sophon-stream filter element是sophon-stream框架中的一个插件，是一个专用作数据筛选得工具。
+
+## 1. 配置参数
+sophon-stream http_push插件具有一些可配置的参数，可以根据需求进行设置。以下是一些常用的参数：
+
+```json
+{
+    "configure": {
+        "rules": [
+            {
+                "channel_id": 0,
+                "filters": [
+                    {
+                        "alert_first_frames": 0,
+                        "alert_frame_skip_nums": 6,
+                        "areas": [
+                            [
+                                {
+                                    "top": 0,
+                                    "left": 0
+                                },
+                                {
+                                    "top": 0,
+                                    "left": 100000
+                                },
+                                {
+                                    "top": 1000000,
+                                    "left": 1000000
+                                },
+                                {
+                                    "top": 100000,
+                                    "left": 0
+                                }
+                            ]
+                        ],
+                        "classes": [
+                            0
+                        ],
+                        "times": [
+                            {
+                                "time_start": "00 00 00",
+                                "time_end": "23 59 59"
+                            }
+                        ],
+                        "type": 0
+                    }
+                ]
+            }
+        ]
+    },
+    "shared_object": "../../build/lib/libfilter.so",
+    "name": "filter",
+    "side": "sophgo",
+    "thread_number": 1
+}
+```
+
+| 参数名        | 类型   | 默认值                               | 说明                            |
+| ------------- | ------ | ------------------------------------ | ------------------------------- |
+| channel_id            | int | 0                           | 顺序和id与demo得json一致          |
+| alert_first_frames            | int | 0                       | 每一路追踪到第几帧开始上报               |
+| alert_frame_skip_nums           | int | 1                    | 从第一次上报开始每几帧上报一次          |
+| top           | int | 1                    | 多边形顶点得x坐标,需要满足循序，个数为0代表全图检测，1或2会发出警告    |
+| left           | int | 1                    | 多边形顶点得y坐标,需要满足循序，个数为0代表全图检测，1或2会发出警告    |
+| classes           | list[int] | []                    | 筛选的类别，为空将会全部筛掉        |
+| time_start           | string | 无                    | 开始时间，格式hh mm ss，为空将会全部筛掉  |
+| time_end           | int | 无                   | 结束时间，格式hh mm ss,为空将会全部筛掉    |
+| type           | int | 0                    | 筛选类型，track:0        |
+| shared_object | string | "../../../build/lib/libhttp_push.so" | libhttp_push动态库路径          |
+| name          | string | "http_push"                          | element名称                     |
+| side          | string | "sophgo"                             | 设备类型                        |
+| thread_number | int    | 1                                    | 启动线程数                      |
+
+
---
+++ element/tools/filter/include/filter.h
@@ -0,0 +1,111 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// SOPHON-STREAM is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SOPHON_STREAM_ELEMENT_FILTER_H_
+#define SOPHON_STREAM_ELEMENT_FILTER_H_
+
+#include <nlohmann/json.hpp>
+#include <queue>
+
+#include "common/common_defs.h"
+#include "common/logger.h"
+#include "common/object_metadata.h"
+#include "element_factory.h"
+namespace sophon_stream {
+namespace element {
+namespace filter {
+struct Area {
+  std::vector<common::Point<int>> points;
+};
+class Filter_Imp {
+ public:
+  Filter_Imp(){};
+  bool isInPolygon(std::shared_ptr<common::ObjectMetadata> objectMetadata);
+  bool isinclasses(std::shared_ptr<common::ObjectMetadata> objectMetadata);
+  bool isOutsideWorkingHours(
+      std::shared_ptr<common::ObjectMetadata> objectMetadata);
+  bool istrack(std::shared_ptr<common::ObjectMetadata> objectMetadata,
+               std::unordered_map<std::string, int>& continue_frame_num_);
+  void push_class(int Class) { classes.push_back(Class); };
+  void set_alert_first_frames(int alert_first_frames_) {
+    alert_first_frames = alert_first_frames_;
+  };
+  void set_alert_frame_skip_nums(int alert_frame_skip_nums_) {
+    alert_frame_skip_nums = alert_frame_skip_nums_;
+  };
+  void push_time(std::pair<int64_t, int64_t> time) { times.push_back(time); };
+  void push_area(Area area) { areas.push_back(area); };
+  void set_type(int type_) { type = type_; };
+
+ private:
+  std::vector<int> classes;
+  int alert_first_frames;
+  int alert_frame_skip_nums;
+  std::vector<std::pair<int64_t, int64_t>> times;
+  std::vector<Area> areas;
+  int type;  // 筛选类型
+
+  bool onSegment(const common::Point<int>& p, const common::Point<int>& q,
+                 const common::Point<int>& r);
+
+  int orientation(const common::Point<int>& p, const common::Point<int>& q,
+                  const common::Point<int>& r);
+
+  bool doIntersect(const common::Point<int>& p1, const common::Point<int>& q1,
+                   const common::Point<int>& p2, const common::Point<int>& q2);
+
+  bool isPointInsidePolygon(const common::Point<int>& p,
+                            const std::vector<common::Point<int>>& polygon);
+
+  bool isRectangleInsidePolygon(std::vector<common::Point<int>>& rectangle,
+                                const std::vector<common::Point<int>>& polygon);
+};
+class Filter : public ::sophon_stream::framework::Element {
+ public:
+  Filter();
+  ~Filter() override;
+  common::ErrorCode initInternal(const std::string& json) override;
+
+  common::ErrorCode doWork(int dataPipeId) override;
+
+  static constexpr const char* CONFIG_INTERNAL_RULES_FILED = "rules";
+  static constexpr const char* CONFIG_INTERNAL_CHANNEL_ID_FILED = "channel_id";
+  static constexpr const char* CONFIG_INTERNAL_FILTERS_FILED = "filters";
+
+  static constexpr const char* CONFIG_INTERNAL_ALERT_FIRST_FRAME_FILED =
+      "alert_first_frame";
+  static constexpr const char* CONFIG_INTERNAL_ALERT_FRAME_SKIP_NUM_FILED =
+      "alert_frame_skip_nums";
+  static constexpr const char* CONFIG_INTERNAL_AREAS_FILED = "areas";
+
+  static constexpr const char* CONFIG_INTERNAL_TOP_FILED = "top";
+  static constexpr const char* CONFIG_INTERNAL_LEFT_FILED = "left";
+  static constexpr const char* CONFIG_INTERNAL_CLASSES_FILED = "classes";
+  static constexpr const char* CONFIG_INTERNAL_TIMES_FILED = "times";
+  static constexpr const char* CONFIG_INTERNAL_TIME_START_FILED = "time_start";
+  static constexpr const char* CONFIG_INTERNAL_TIME_END_FILED = "time_end";
+  static constexpr const char* CONFIG_INTERNAL_TYPE_FILED = "type";
+
+ private:
+  int64_t timeToMilliseconds(
+      const std::string& time);  // 把时间戳转换成一天内的时间
+
+  std::vector<std::vector<Filter_Imp>> Filter_imps;  // 不同路的不同过滤器
+  std::unordered_map<int, int>
+      channel_id_indexs;  // 每一路对应的channel_id的index
+  std::vector<std::unordered_map<std::string, int>>
+      continue_frame_num;  // 每一路连续追踪计数，map中key代表追踪的id,value代表追踪帧数
+  // std::mutex condit_mtx;
+};
+
+}  // namespace filter
+}  // namespace element
+}  // namespace sophon_stream
+
+#endif
